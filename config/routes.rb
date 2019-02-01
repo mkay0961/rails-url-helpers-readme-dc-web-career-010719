eea54689430805daa6eab1f27753530aeab0c412
mkay0961 <mkay0961@gmail.com>
2019-02-01 11:52:11 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   resources :posts, only: [:index, :show]
+
+  # get "/students/:id", to: "students#"
+  #
+  # get "/students", to: "students#index"
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Post.create(title: "sdfasdf", description: "sakjdfhsa")
+second_post = Post.create(title: "My Title", description: "My post description")
